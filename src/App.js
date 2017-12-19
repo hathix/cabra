b27487f5f0a1faddaa5fdd76d8f58009726f1719
Neel Mehta <neelmehta96@gmail.com>
2017-12-19 17:02:50 -0500

    finish refactoring in

--- src/App.js
+++ src/App.js
@@ -1,89 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-import HomeView from './views/HomeView';
-import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
+import { Provider, connect } from 'react-redux'
 import { Route } from 'react-router'
-import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux';
-import { Provider, connect } from 'react-redux';
+import {createStore, combineReducers, applyMiddleware } from 'redux'
+import createHistory from 'history/createBrowserHistory'
+import React, { Component } from 'react'
 
-import {createStore, combineReducers, applyMiddleware } from 'redux';
+// import logo from './logo.svg'
+import './App.css'
+import HomeView from './views/HomeView'
 
+// Demo decks
+// let initialDecks = [ {
+//     name: "apple"
+//   }, {
+//     name: "banana"
+//   }];
 
+// redux actions
+let ACTION_TYPES = {
+  ADD_DECK: 'ADD_DECK'
+}
 
-let initialDecks = [ {
-    name: "apple"
-  }, {
-    name: "banana"
-  }];
-
-const ACTION_TYPES = {
-  ADD_DECK: 'ADD_DECK',
-  NO_OP: 'NO_OP'
-};
-
-// action creators
-const addDeck = (deck={name: ''}) => ({
+let actions = {
+  addDeck: (deck={name: ''}) => ({
     type: ACTION_TYPES.ADD_DECK,
-    deck
-});
+    deck: deck
+  })
+}
 
-let deckReducer = function(state=initialDecks, action) {
+// handles all deck-related actions
+let deckReducer = function(state=[], action) {
   switch (action.type) {
     case ACTION_TYPES.ADD_DECK:
       // TODO do all this with immutable.js tbh
       return [
         ...state,
         action.deck
-      ];
-
-    case ACTION_TYPES.NO_OP:
-      return state;
+      ]
   }
 
   // default: do nothing
-  return state;
+  return state
 }
 
 // React Router stuff
 // Create a history of your choosing (we're using a browser history in this case)
-const history = createHistory();
+const history = createHistory()
 
 // Build the middleware for intercepting and dispatching navigation actions
-const middleware = routerMiddleware(history);
+const middleware = routerMiddleware(history)
 
 // set up routers. add the reducer to store on 'router' key
-
 let reducers = {
   decks: deckReducer,
   router: routerReducer
-};
+}
 
 // Also apply our middleware for navigating
 const store = createStore(
   combineReducers(reducers),
-  // initialState,
   applyMiddleware(middleware)
-);
-
+)
 
 let mapStateToProps = function(state) {
   return {
     decks: state.decks
-  };
-};
+  }
+}
 
-export { mapStateToProps };
-
-
-store.dispatch(addDeck({
+// for testing, add a new deck
+store.dispatch(actions.addDeck({
   // a new deck
   name: "cherry"
-}));
+}))
 
 // wrap components with the connector so they can access state
-let HomeView2 = connect(mapStateToProps)(HomeView);
+let HomeView2 = connect(mapStateToProps)(HomeView)
 
 class App extends Component {
   render() {
@@ -102,11 +94,11 @@ class App extends Component {
         </Provider>
         </div>
       </div>
-    );
+    )
   }
-};
+}
 
 // To programatically go to a certain URL
 // store.dispatch(push('/about'));
 
-export default App;
+export default App
